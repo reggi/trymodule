8b1a3d537cc8472a97870935692e348691fff79a
Victor Bjelkholm <victor@typeform.com>
2016-03-21 18:01:17 -0500

    Split CLI and install modules stuff

---
+++ cli.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+
+var repl = require('repl')
+var path = require('path')
+var colors = require('colors')
+var replHistory = require('repl.history')
+var exec = require('child_process').exec
+var loadPackages = require('./index')
+
+const TRYMODULE_PATH = process.env.TRYMODULE_PATH || path.resolve((process.env.HOME || process.env.USERPROFILE), '.trymodule')
+const TRYMODULE_HISTORY_PATH = process.env.TRYMODULE_HISTORY_PATH || path.resolve(TRYMODULE_PATH, 'repl_history')
+
+if (process.argv[2] === undefined) {
+  throw new Error('You need to provide package name as first argument')
+}
+
+const logGreen = (msg) => console.log(colors.green(msg))
+
+const takePackageArguments = (args) => args.splice(2)
+
+const addPackageToObject = (obj, pkg) => {
+  const variable_friendly_package_name = pkg.name.replace(/-|\./g, '_')
+  logGreen(`Package '${pkg.name}' was loaded and assigned to '${variable_friendly_package_name}' in the current scope`)
+  obj[variable_friendly_package_name] = pkg.package
+  return obj
+}
+
+if (process.argv[2] === '--clear') {
+  console.log(`Removing folder ${TRYMODULE_PATH + '/node_modules'}`)
+  exec('rm -r ' + TRYMODULE_PATH + '/node_modules', (err, stdout, stderr) => {
+    if (!err) {
+      logGreen('Cache successfully cleared!')
+      process.exit(0)
+    } else {
+      throw new Error('Could not remove cache! Error ' + err)
+    }
+  })
+} else {
+  logGreen('Gonna start a REPL with packages installed and loaded for you')
+  const packages_to_install = takePackageArguments(process.argv)
+
+  // Extract
+  loadPackages(packages_to_install, TRYMODULE_PATH).then((packages) => {
+    const context_packages = packages.reduce((context, pkg) => {
+      return addPackageToObject(context, pkg)
+    }, {})
+    console.log('REPL started...')
+    if (!process.env.TRYMODULE_NONINTERACTIVE) {
+      var replServer = repl.start({
+        prompt: '> '
+      })
+      replHistory(replServer, TRYMODULE_HISTORY_PATH)
+      replServer.context = Object.assign(replServer.context, context_packages)
+    }
+  })
+}
--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 #!/usr/bin/env node
-var repl = require('repl')
+
 var npmi = require('npmi')
 var path = require('path')
 var colors = require('colors')
-var replHistory = require('repl.history')
-var exec = require('child_process').exec
 
-const TRYMODULE_PATH = process.env.TRYMODULE_PATH || path.resolve((process.env.HOME || process.env.USERPROFILE), '.trymodule')
-const TRYMODULE_HISTORY_PATH = process.env.TRYMODULE_HISTORY_PATH || path.resolve(TRYMODULE_PATH, 'repl_history')
+const packageLocation = (pkg, trymodule_path) => path.resolve(trymodule_path, 'node_modules', pkg)
 
-if (process.argv[2] === undefined) {
-  throw new Error('You need to provide package name as first argument')
-}
-
-const logGreen = (msg) => console.log(colors.green(msg))
-
-const takePackageArguments = (args) => args.splice(2)
-
-const packageLocation = (pkg) => path.resolve(TRYMODULE_PATH, 'node_modules', pkg)
-
-const loadPackage = (pkg) => {
+const loadPackage = (pkg, trymodule_path) => {
   return new Promise((resolve, reject) => {
     try {
-      const loadedPackage = require(packageLocation(pkg))
+      const loadedPackage = require(packageLocation(pkg, trymodule_path))
       console.log(colors.blue(`'${pkg}' was already installed since before!`))
       resolve({name: pkg, package: loadedPackage})
     } catch (err) {
       console.log(colors.yellow(`Couldn't find '${pkg}' locally, gonna download it now`))
-      npmi({name: pkg, path: TRYMODULE_PATH}, (err, result) => {
+      npmi({name: pkg, path: trymodule_path}, (err, result) => {
         if (err) {
           console.log(colors.red(err.message))
           if (err.statusCode === 404) {
@@ -40,46 +27,16 @@ const loadPackage = (pkg) => {
             throw new Error('npm install error')
           }
         }
-        const loadedPackage = require(packageLocation(pkg))
+        const loadedPackage = require(packageLocation(pkg, trymodule_path))
         resolve({name: pkg, package: loadedPackage})
       })
     }
   })
 }
 
-const addPackageToObject = (obj, pkg) => {
-  const variable_friendly_package_name = pkg.name.replace(/-|\./g, '_')
-  logGreen(`Package '${pkg.name}' was loaded and assigned to '${variable_friendly_package_name}' in the current scope`)
-  obj[variable_friendly_package_name] = pkg.package
-  return obj
-}
-
-if (process.argv[2] === '--clear') {
-  console.log(`Removing folder ${TRYMODULE_PATH + '/node_modules'}`)
-  exec('rm -r ' + TRYMODULE_PATH + '/node_modules', (err, stdout, stderr) => {
-    if (!err) {
-      logGreen('Cache successfully cleared!')
-      process.exit(0)
-    } else {
-      throw new Error('Could not remove cache! Error ' + err)
-    }
-  })
-} else {
-  logGreen('Gonna start a REPL with packages installed and loaded for you')
-  const packages_to_install = takePackageArguments(process.argv)
-  const promises_for_installation = packages_to_install.map((package_name) => loadPackage(package_name))
-
-  Promise.all(promises_for_installation).then((packages) => {
-    const context_packages = packages.reduce((context, pkg) => {
-      return addPackageToObject(context, pkg)
-    }, {})
-    console.log('REPL started...')
-    if (!process.env.TRYMODULE_NONINTERACTIVE) {
-      var replServer = repl.start({
-        prompt: '> '
-      })
-      replHistory(replServer, TRYMODULE_HISTORY_PATH)
-      replServer.context = Object.assign(replServer.context, context_packages)
-    }
+module.exports = (packages_to_install, trymodule_path) => {
+  return new Promise((resolve, reject) => {
+    const promises_for_installation = packages_to_install.map((package_name) => loadPackage(package_name, trymodule_path))
+    Promise.all(promises_for_installation).then(resolve).catch(reject)
   })
 }
