457e365eb3ca9c207a3b3d809baac32175259367
Victor Bjelkholm <victor@typeform.com>
2016-03-21 18:02:40 -0500

    trymodule_path should've been install_path

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var npmi = require('npmi')
 var path = require('path')
 var colors = require('colors')
 
-const packageLocation = (pkg, trymodule_path) => path.resolve(trymodule_path, 'node_modules', pkg)
+const packageLocation = (pkg, install_path) => {
+  path.resolve(install_path, 'node_modules', pkg)
+}
 
-const loadPackage = (pkg, trymodule_path) => {
+const loadPackage = (pkg, install_path) => {
   return new Promise((resolve, reject) => {
     try {
-      const loadedPackage = require(packageLocation(pkg, trymodule_path))
+      const loadedPackage = require(packageLocation(pkg, install_path))
       console.log(colors.blue(`'${pkg}' was already installed since before!`))
       resolve({name: pkg, package: loadedPackage})
     } catch (err) {
       console.log(colors.yellow(`Couldn't find '${pkg}' locally, gonna download it now`))
-      npmi({name: pkg, path: trymodule_path}, (err, result) => {
+      npmi({name: pkg, path: install_path}, (err, result) => {
         if (err) {
           console.log(colors.red(err.message))
           if (err.statusCode === 404) {
@@ -27,16 +29,18 @@ const loadPackage = (pkg, trymodule_path) => {
             throw new Error('npm install error')
           }
         }
-        const loadedPackage = require(packageLocation(pkg, trymodule_path))
+        const loadedPackage = require(packageLocation(pkg, install_path))
         resolve({name: pkg, package: loadedPackage})
       })
     }
   })
 }
 
-module.exports = (packages_to_install, trymodule_path) => {
+module.exports = (packages_to_install, install_path) => {
   return new Promise((resolve, reject) => {
-    const promises_for_installation = packages_to_install.map((package_name) => loadPackage(package_name, trymodule_path))
+    const promises_for_installation = packages_to_install.map((package_name) => {
+      loadPackage(package_name, install_path)
+    })
     Promise.all(promises_for_installation).then(resolve).catch(reject)
   })
 }
